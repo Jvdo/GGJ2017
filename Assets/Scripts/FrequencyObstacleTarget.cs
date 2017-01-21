94f52a8a1d8d4861e971305c20a1d2e96cb8cb81
Jeroen Matser <mail@jmatser.com>
2017-01-21 02:58:02 +0100

    Added frequency obstacle

---
+++ Assets/Scripts/FrequencyObstacleTarget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FrequencyObstacleTarget : MonoBehaviour {
+
+	FrequencyObstacle parent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!parent)
+		{
+			parent = transform.GetComponentInParent<FrequencyObstacle>();
+		}
+	}
+
+	void OnDrawGizmos() {
+		
+		if (parent)
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(transform.position, 0.5f);
+			Gizmos.DrawLine(transform.position, parent.transform.position);
+		}
+	}
+}
