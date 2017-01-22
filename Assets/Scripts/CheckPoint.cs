4f90b9d551ffb1412ee421f93546ef3780ba29cc
Daniel <Daniel.de.Luca@hva.nl>
2017-01-22 02:33:20 +0100

    no message

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour {
 
+
+    public Sprite red;
+    public Sprite green;
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.tag == "Player")
         {
             coll.GetComponent<characterMovement>().SetSpawnPos(transform.position);
+            GetComponentsInChildren<SpriteRenderer>()[1].sprite = green;
         }
     }
 }
