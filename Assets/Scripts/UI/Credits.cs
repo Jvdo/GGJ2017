82757a6febc004b7ab9ca846888e70e7d65bd7e6
Jeroen Matser <mail@jmatser.com>
2017-01-22 12:40:58 +0100

    Added credits.

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	public float translationTime = 10.0f;
+	public float totalTranslation = 2000.0f;
+	public float totalTime = 14.0f;
+
+	float currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		float t = Mathf.Clamp01(currentTime / translationTime);
+		float delta = Mathf.Lerp(0, totalTranslation, t);
+		transform.localPosition = new Vector3(0, delta, 0);
+
+		if (currentTime >= totalTime)
+		{
+			SceneManager.LoadScene("MainMenu");
+		}
+	}
+}
